45e937f5fe52fa1bd5c939442df6acc89fdae494
gravitana <v.a.grahov@ya.ru>
2021-02-15 16:33:13 +0500

    Homework3

--- src/main/java/homework3/Task_3_1.java
+++ src/main/java/homework3/Task_3_1.java
@@ -1,6 +1,7 @@
 package homework3;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Task_3_1 {
     /*
@@ -24,6 +25,56 @@ public class Task_3_1 {
                 "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut",
                 "pear", "pepper", "pineapple", "pumpkin", "potato",
         };
+        Random rand = new Random();
+        int index = rand.nextInt(words.length);
+        String word = words[index];
+
+        boolean gameOver = false;
+
+        System.out.println("Угадайте слово, которое загадал компьютер.");
+
+        while (!gameOver) {
+            System.out.println("Варианты ответов:");
+            for (int i = 0; i < words.length; i++) {
+                System.out.print(words[i] + ", ");
+                if ((i + 1) % 10 == 0)
+                    System.out.println();
+            }
+            System.out.println();
+            System.out.println("Введите ответ");
+
+            String userAnswer = getAnswer();
+            gameOver = userAnswer.equals(word);
+
+            if (gameOver)
+                System.out.println("Слово отгадано!");
+            else
+                hintAnswer(userAnswer, word);
+        }
     }
 
+    private static String getAnswer() {
+        Scanner scan = new Scanner(System.in);
+        return scan.next();
+    }
+
+    private static void hintAnswer(String userAnswer, String word) {
+        System.out.println("Попробуйте ещё раз.");
+
+        int len = userAnswer.length() < word.length() ? userAnswer.length() : word.length();
+
+        System.out.println();
+
+        for (int i = 0; i < 15; i++) {
+            if (i < len) {
+                char w = word.charAt(i);
+                char u = userAnswer.charAt(i);
+                System.out.print(w == u ? w : "#");
+            }
+            else
+                System.out.print("#");
+        }
+        System.out.println();
+        System.out.println();
+    }
 }
