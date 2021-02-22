b6448c7d3393b27951a19b735e55b338ba55cab2
Vitaliy <v.a.grahov@ya.ru>
2021-02-22 22:45:26 +0500

    homework5

--- src/main/java/lesson5/am/Main.java
+++ src/main/java/lesson5/am/Main.java
@@ -2,6 +2,8 @@ package lesson5.am;
 
 public class Main {
     public static void main(String[] args) {
+
+/*
         Cat cat1 = new Cat("Мартин Лютер Кот", "Афрокот", 2);
         Cat cat2 = new Cat("Борис Николаевич", "Бурый");
         Cat cat3 = new Cat("Шава");
@@ -29,5 +31,20 @@ public class Main {
         }
 
         System.out.println(Cat.getCatsCount());
+*/
+
+        /* Домашнее задание */
+        Person[] persArray = {
+                new Person("Иванов Иван Иванович", "Инженер", "iii@example.com", "+7 (123) 456-78-90", 50000, 50),
+                new Person("Петров Пётр Петрович", "Специалист", "ppp@example.com", "+7 (123) 456-78-91", 30000, 30),
+                new Person("Горбунков Семён Семёнович", "Старший экономист", "gss@example.com", "+7 (123) 456-78-92", 35000, 47),
+                new Person("Бунша Иван Васильевич", "Управдом", "biv@example.com", "+7 (123) 456-78-93", 20000, 45),
+                new Person("Бендер Остап Ибрагимович", "Великий комбинатор", "boi@example.com", "+7 (123) 456-78-94", 40000, 27),
+        };
+
+        for (Person person : persArray) {
+            if (person.age > 40)
+                person.printInfo();
+        }
     }
 }
