d8e163a04aa064959a36a94615ff0a7b35ccf1f0
gravitana <v.a.grahov@ya.ru>
2021-03-04 10:51:43 +0500

    homework7

--- src/main/java/lesson7/oop/Cat.java
+++ src/main/java/lesson7/oop/Cat.java
@@ -5,12 +5,27 @@ import java.util.Random;
 public class Cat {
     private String name;
     private Random random = new Random();
+    private int appetite;
+    private boolean satiety = false;
 
     public Cat(String name) {
         this.name = name;
+        this.appetite = random.nextInt(9) + 3;
     }
 
     public void eat(Plate plate) {
-        plate.decreaseFood(random.nextInt(4) + 3);
+        if (satiety) {
+            System.out.println("Кот " + name + " уже сыт");
+        } else if (plate.decreaseFood(appetite)) {
+            System.out.println("Кот " + name + " съел " + appetite);
+            satiety = true;
+        } else {
+            System.out.println("Еды не хватило! Нужно " + appetite);
+        }
     }
+
+    public void printInfo() {
+        System.out.printf("Кот %s — %s%n", name, (satiety ? "сытый" : "голодный"));
+    }
+
 }
