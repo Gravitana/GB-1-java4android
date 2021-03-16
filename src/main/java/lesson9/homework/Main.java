1140abd76ffaa3c36e8a154e3bbe43c2587fc111
Vitaliy <v.a.grahov@ya.ru>
2021-03-17 00:10:17 +0500

    homework9 part1 random string lenght

--- src/main/java/lesson9/homework/Main.java
+++ src/main/java/lesson9/homework/Main.java
@@ -4,37 +4,46 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) throws MyArraySizeException {
-        final int etaloneSize = 4;
+        int standardArraySize = 4;
+        int maxStringLength = 6;
 
-//        int size = 4;
-        int size = 6;
+        int size = 4;
+//        int size = 6;
 
         var chars = "0123456789".toCharArray();
 //        var chars = "0123456789abc@#$".toCharArray();
 
         String[][] myArray = new String[size][size];
 
-        fillArray(chars, myArray);
+        fillArray(myArray, chars, maxStringLength);
 
         printArray(myArray);
 
-        arrayProcessing(myArray, etaloneSize);
+        arrayProcessing(myArray, standardArraySize);
 
     }
 
-    private static void fillArray(char[] chars, String[][] myArray) {
-        Random rand = new Random();
+    private static void fillArray(String[][] myArray, char[] chars, int maxStringLength) {
         for (int i = 0; i < myArray.length; i++) {
             for (int j = 0; j < myArray[i].length; j++) {
-                myArray[i][j] = String.valueOf(chars[rand.nextInt(chars.length)]);
+                myArray[i][j] = generateRandomString(chars, maxStringLength);
             }
         }
     }
 
+    private static String generateRandomString(char[] chars, int maxLength) {
+        Random rand = new Random();
+        StringBuilder string = new StringBuilder();
+        for (int i = 0; i <= rand.nextInt(maxLength); i++) {
+            string.append(chars[rand.nextInt(chars.length)]);
+        }
+        return string.toString();
+    }
+
     private static void printArray(String[][] someArray) {
-        for (int i = 0; i < someArray.length; i++) {
-            for (int j = 0; j < someArray[i].length; j++) {
-                System.out.printf("%s", someArray[i][j]);
+        for (String[] strings : someArray) {
+            for (String string : strings) {
+                System.out.print(string + " ");
             }
             System.out.println();
         }
