cf6f1165c72e3d7346defabb68b3c319331e0a80
gravitana <v.a.grahov@ya.ru>
2021-03-24 16:41:21 +0500

    lesson12 DeadLock

--- src/main/java/lesson12/DeadLock.java
+++ src/main/java/lesson12/DeadLock.java
@@ -2,4 +2,49 @@ package lesson12;
 
 public class DeadLock {
 
+    static final Object lock1 = new Object();
+    static final Object lock2 = new Object();
+
+    public static void main(String[] args) {
+        DeadThreadOne one = new DeadThreadOne();
+        one.start();
+        DeadThreadTwo two = new DeadThreadTwo();
+        two.start();
+    }
+
+    static class DeadThreadOne extends Thread {
+        @Override
+        public void run() {
+            synchronized (lock1) {
+                System.out.println("One держит lock1...");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("One ждёт lock2...");
+                synchronized (lock2) {
+                    System.out.println("One держит lock1 и lock2...");
+                }
+            }
+        }
+    }
+
+    static class DeadThreadTwo extends Thread {
+        @Override
+        public void run() {
+            synchronized (lock2) {
+                System.out.println("Two держит lock2...");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Two ждёт lock1...");
+                synchronized (lock1) {
+                    System.out.println("Two держит lock2 и lock1...");
+                }
+            }
+        }
+    }
 }
