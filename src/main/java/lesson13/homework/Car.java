dc8c122503975a15fe1e04fe5e2910124c4db680
Vitaliy Grakhov <v.a.grahov@ya.ru>
2021-03-29 18:00:37 +0500

    Homework13 (#13)
    * homework13 перед туннелем
    
    * homework13 перед победителем
    
    * homework13 победитель не тот

--- src/main/java/lesson13/homework/Car.java
+++ src/main/java/lesson13/homework/Car.java
@@ -1,5 +1,8 @@
 package lesson13.homework;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
     private Race race;
@@ -14,11 +17,19 @@ public class Car implements Runnable {
         return speed;
     }
 
-    public Car(Race race, int speed) {
+    CountDownLatch finishedAll;
+    CyclicBarrier startLine;
+    CountDownLatch winner;
+
+
+    public Car(Race race, int speed, CountDownLatch cdl, CyclicBarrier cb, CountDownLatch w) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
+        finishedAll = cdl;
+        startLine = cb;
+        winner = w;
     }
 
     @Override
@@ -27,11 +38,17 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int) (Math.random() * 800));
             System.out.println(this.name + " готов");
+            startLine.await();
         } catch (Exception e) {
             e.printStackTrace();
         }
+
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
         }
+
+        finishedAll.countDown();
+
+        winner.countDown();
     }
 }
